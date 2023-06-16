566924f37dd7d89a20a56f9507e5e75dba3021c3
Ollie Boyne <48369287+OllieBoyne@users.noreply.github.com>
2023-06-16 15:40:39 +0100

    Cleaned up junk
    Co-authored-by: ollieparish <ollieparish@users.noreply.github.com>

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -1,22 +1,8 @@
-import React, { forwardRef, useImperativeHandle, useRef } from 'react';
-
-import MiniPlayer from '../../components/mini-player';
-import { CONTROLS, POSITION } from '../../components/mini-player';
-import Button from 'react';
-import useStream from '../../components/Stream';
-import { getConfigFromResolution } from '../../components/Helpers';
-import IVSBroadcastClient, {
-  Errors,
-  BASIC_LANDSCAPE
-} from 'amazon-ivs-web-broadcast';
+import React, { useRef } from 'react';
 import { GoogleMap, LoadScript, Marker} from '@react-google-maps/api'
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-//import './HomePage.css';
 import '../../App.css';
 
-// const STREAM_PLAYBACK_URL = 'https://c6d0b5e2ec90.eu-west-1.playback.live-video.net/api/video/v1/eu-west-1.449365895007.channel.YMQwDQyyYa0W.m3u8';
-//'https://6547df1ee2a7.eu-west-1.playback.live-video.net/api/video/v1/eu-west-1.832590881550.channel.zuXw8aqr1sZO.m3u8'
 const GOOGLE_MAPS_API_KEY = 'AIzaSyDpcl7prQQADOD4o_jRuWSsnD79kGvPBMw';
 
 const containerStyle = {
@@ -58,107 +44,6 @@ function MapWithMarker() {
 function HomePage() {
     const ref = useRef();
     const history = useNavigate();
-  
-    async function handlePermissions() {
-      let permissions = {
-        audio: false,
-        video: false,
-      };
-      try {
-        const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
-        for (const track of stream.getTracks()) {
-          track.stop();
-        }
-        permissions = { video: true, audio: true };
-      } catch (err) {
-        permissions = { video: false, audio: false };
-        console.error(err.message);
-      }
-      // If we still don't have permissions after requesting them display the error message
-      if (!permissions.video) {
-        console.error('Failed to get video permissions.');
-      } else if (!permissions.audio) {
-        console.error('Failed to get audio permissions.');
-      }
-    }
-  
-    var client = null
-  
-    async function Initialize() {
-  
-      const streamConfig = IVSBroadcastClient.BASIC_LANDSCAPE;
-  
-      client = IVSBroadcastClient.create({
-        // Enter the desired stream configuration
-        streamConfig: streamConfig,
-        // Enter the ingest endpoint from the AWS console or CreateChannel API
-        ingestEndpoint: "rtmps://c6d0b5e2ec90.global-contribute.live-video.net:443/app/",
-        streamKey: "sk_eu-west-1_Ooyab7a0i7Z3_BuUAqkJoyeQsdi6lOEf4gfdRtGDYDL",
-      });
-  
-      // Get geolocation as .json
-      const position = await fetchGeolocationData();
-      const position_dict = { "latitude": position.coords.latitude, "longitude": position.coords.longitude };
-  
-      const devices = await navigator.mediaDevices.enumerateDevices();
-      window.videoDevices = devices.filter((d) => d.kind === 'videoinput');
-      window.audioDevices = devices.filter((d) => d.kind === 'audioinput');
-  
-      window.cameraStream = await navigator.mediaDevices.getUserMedia({
-        video: {
-          deviceId: window.videoDevices[0].deviceId,
-          width: {
-            ideal: streamConfig.maxResolution.width,
-            max: streamConfig.maxResolution.width,
-          },
-          height: {
-            ideal: streamConfig.maxResolution.height,
-            max: streamConfig.maxResolution.height,
-          },
-        },
-      });
-  
-      window.microphoneStream = navigator.mediaDevices.getUserMedia({
-        audio: { deviceId: window.audioDevices[0].deviceId },
-      });
-      console.log(window.microphoneStream)
-      console.log(window.cameraStream)
-      client.addVideoInputDevice(window.cameraStream, 'camera1', { index: 0 }); // only 'index' is required for the position parameter
-      client.addAudioInputDevice(window.microphoneStream, 'mic1');
-    }
-  
-    Initialize()
-  
-  
-    const handleStream = async () => {
-      handlePermissions()
-      // toggleStream(
-      //   client.ingestEndpoint,//TODO TO ingest Serbver
-      //   "", //TODO To streamKey 
-      //   'BASIC', // `channel.type`
-      //   client.current, // `client.current`
-      //   handleError
-      // );
-      client
-        .startBroadcast("sk_eu-west-1_Ooyab7a0i7Z3_BuUAqkJoyeQsdi6lOEf4gfdRtGDYDL")
-        .then((result) => {
-          console.log('I am successfully broadcasting!');
-          //ref.current.log();
-        })
-        .catch((error) => {
-          console.error('Something drastically failed while broadcasting!', error);
-        });
-  
-    };
-  
-    const handleNoStream = async () => {
-      client.stopBroadcast()
-      console.log("Ended stream");
-    }
-  
-    const refreshStream = async () => {
-      ref.current.log();
-    }
   
     return (
       <div>
