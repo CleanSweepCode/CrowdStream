2acd6d05af22f10f57900626270a56b5c62b522c
Ollie Boyne <48369287+OllieBoyne@users.noreply.github.com>
2023-06-16 17:15:51 +0100

    Forwarding links

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -2,6 +2,7 @@ import React, { useRef } from 'react';
 import { GoogleMap, LoadScript, Marker} from '@react-google-maps/api'
 import { useNavigate } from "react-router-dom";
 import '../../App.css';
+import { listChannels } from '../utils.jsx'
 
 const GOOGLE_MAPS_API_KEY = 'AIzaSyDpcl7prQQADOD4o_jRuWSsnD79kGvPBMw';
 
@@ -9,32 +10,56 @@ const containerStyle = {
   width: '100%',
   height: '400px'
 };
-const center = {
+
+var center = {
   lat: 51.46931506612955,
   lng: -0.21997342925326427
 };
 
-const markerPosition = {
+var markerPosition = {
   lat: 51.46931506612955,
   lng: -0.21997342925326427
 };
 
-<Marker position={markerPosition} />
+// initialize marker position as channel 0 
+let channel_info = await listChannels();
+console.log(channel_info);
+
+const channel0 = {
+  lat: parseFloat(channel_info[0].tags.latitude),
+  lng: parseFloat(channel_info[0].tags.longitude)
+};
+
+center = channel0;
+markerPosition = channel0;
 
 // Custom Map Component
 function MapWithMarker() {
   const navigate = useNavigate();
 
+
   return (
     <GoogleMap
       mapContainerStyle={containerStyle}
       center={center}
-      zoom={10}
+      zoom={8}
     >
-      <Marker 
+
+    {channel_info.map((channel, index) => (
+            <Marker 
+              key={index}
+              position={{
+                lat: parseFloat(channel.tags.latitude),
+                lng: parseFloat(channel.tags.longitude)
+              }} 
+              onClick={() => navigate(`/NewPage/${channel.name}`)} 
+            />
+          ))}
+
+      {/* <Marker 
         position={center} 
         onClick={() => navigate('/NewPage')} 
-      />
+      /> */}
 
     </GoogleMap>
   )
