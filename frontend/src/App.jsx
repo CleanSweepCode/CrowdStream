bb30eb928b517dbb2588188f535eaac4694ea366
Ollie Boyne <48369287+OllieBoyne@users.noreply.github.com>
2023-06-19 16:12:54 +0100

    New streamer page + create channel functionality
    Co-authored-by: ollieparish <ollieparish@users.noreply.github.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { forwardRef, useImperativeHandle, useRef } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Viewer from "./pages/viewer";
+import Streamer from "./pages/streamer";
 import HomePage from "./pages/HomePage";
 import './App.css';
 
@@ -9,6 +10,7 @@ const App = () => {
     <div className="App">
       <Routes>
         <Route path="/*" element={<HomePage />} />
+        <Route path="/streamer" element={<Streamer />} />
         <Route path="/viewer" element={<Viewer />} />
         <Route path="/viewer/:channel_name" element={<Viewer />} />
       </Routes>
