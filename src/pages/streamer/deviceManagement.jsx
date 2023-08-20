b743d83cbd509c8c457ee571eed9bfe502b8366c
nmayhew <66178080+nmayhew@users.noreply.github.com>
2023-08-20 14:31:49 +0100

    testings

--- src/pages/streamer/deviceManagement.jsx
+++ src/pages/streamer/deviceManagement.jsx
@@ -1,12 +1,12 @@
 
 export async function requestCameraPermissions() {
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
-      console.log("Camera permissions granted");
-    } catch (err) {
-      console.error("No cameras available, so no camera permissions granted: ", err);
-    }
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+    console.log("Camera permissions granted");
+  } catch (err) {
+    console.error("No cameras available, so no camera permissions granted: ", err);
   }
+}
 
 class DeviceList {
   constructor(arr) {
@@ -23,73 +23,75 @@ class DeviceList {
     return this.array[this.index].label;
   }
 
-    next() {
-      const value = this.array[this.index];
-      this.index = (this.index + 1) % this.array.length;
-      return value;
-    }
+  next() {
+    console.log("next camera: ", this.array[(this.index + 1) % this.array.length].label)
+    const value = this.array[this.index];
+    console.log("value of new camera: ", value.label)
+    this.index = (this.index + 1) % this.array.length;
+    return value;
   }
+}
 
-  export async function getCameraDevices() {
-    // Return a dictionary of camera devices
-    // front: [all front cameras]
-    // rear: [all rear cameras]
-    // all: [all other cameras]
+export async function getCameraDevices() {
+  // Return a dictionary of camera devices
+  // front: [all front cameras]
+  // rear: [all rear cameras]
+  // all: [all other cameras]
 
-    const devices = await navigator.mediaDevices.enumerateDevices();
-    const videoDevices = devices.filter(device => device.kind === 'videoinput');
-    var cameras = videoDevices;
+  const devices = await navigator.mediaDevices.enumerateDevices();
+  const videoDevices = devices.filter(device => device.kind === 'videoinput');
+  var cameras = videoDevices;
 
-    // sort device list, preferring ones that contain rearKeys
-    let rearKeys = ['rear', 'back', 'environment']
+  // sort device list, preferring ones that contain rearKeys
+  let rearKeys = ['rear', 'back', 'environment']
 
-    videoDevices.sort((a, b) => {
-      const aContainsKey = rearKeys.some(key => a.label.toLowerCase().includes(key));
-      const bContainsKey = rearKeys.some(key => b.label.toLowerCase().includes(key));
-    
-      if (aContainsKey && bContainsKey) return 0; // Both have rearKeys, so retain their order
-      if (aContainsKey) return -1;                // a should come before b
-      if (bContainsKey) return 1;                 // b should come before a
-      return 0;                                  // Neither have rearKeys, so retain their order
-    });
+  videoDevices.sort((a, b) => {
+    const aContainsKey = rearKeys.some(key => a.label.toLowerCase().includes(key));
+    const bContainsKey = rearKeys.some(key => b.label.toLowerCase().includes(key));
 
-    var deviceList = new DeviceList(cameras);
-    return deviceList;
+    if (aContainsKey && bContainsKey) return 0; // Both have rearKeys, so retain their order
+    if (aContainsKey) return -1;                // a should come before b
+    if (bContainsKey) return 1;                 // b should come before a
+    return 0;                                  // Neither have rearKeys, so retain their order
+  });
+  console.log(videoDevices);
+  var deviceList = new DeviceList(cameras);
+  return deviceList;
 }
 
 export async function getStreamFromCamera(cameraDevice) {
-    // Given a camera device, return a stream
-    return await navigator.mediaDevices.getUserMedia({
-      video: {
-        deviceId: { exact: cameraDevice.deviceId },
-        width: { ideal: 1280 },
-        height: { ideal: 720 },
-        frameRate: { ideal: 30 }
-      },
-      audio: false
-    });
-  }
+  // Given a camera device, return a stream
+  return await navigator.mediaDevices.getUserMedia({
+    video: {
+      deviceId: { exact: cameraDevice.deviceId },
+      width: { ideal: 1280 },
+      height: { ideal: 720 },
+      frameRate: { ideal: 30 }
+    },
+    audio: false
+  });
+}
 
 export async function handlePermissions() {
-    let permissions = {
-      audio: false,
-      video: false,
-    };
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
-      for (const track of stream.getTracks()) {
-        track.stop();
+  let permissions = {
+    audio: false,
+    video: false,
+  };
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+    for (const track of stream.getTracks()) {
+      track.stop();
 
-      }
-      permissions = { video: true, audio: true };
-    } catch (err) {
-      permissions = { video: false, audio: false };
-      console.error(err.message);
     }
-    // If we still don't have permissions after requesting them display the error message
-    if (!permissions.video) {
-      console.error('Failed to get video permissions.');
-    } else if (!permissions.audio) {
-      console.error('Failed to get audio permissions.');
-    }
+    permissions = { video: true, audio: true };
+  } catch (err) {
+    permissions = { video: false, audio: false };
+    console.error(err.message);
   }
+  // If we still don't have permissions after requesting them display the error message
+  if (!permissions.video) {
+    console.error('Failed to get video permissions.');
+  } else if (!permissions.audio) {
+    console.error('Failed to get audio permissions.');
+  }
+}
