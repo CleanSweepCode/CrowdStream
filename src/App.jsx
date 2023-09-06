a531341be333fe0813dd7601cf3cb1324c8f5f8d
Ollie Boyne <48369287+OllieBoyne@users.noreply.github.com>
2023-09-06 18:01:17 +0100

    Fix to Viewer.jsx deletion

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@
 import React, { forwardRef, useImperativeHandle, useRef } from 'react';
 import { Analytics } from '@vercel/analytics/react';
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import Viewer from "./pages/viewer/viewer.jsx";
 import Streamer from "./pages/streamer/streamer.jsx";
 import HomePage from "./pages/HomePage/HomePage.jsx";
 import InfoPage from "./pages/info/infoPage.jsx";
@@ -15,8 +14,6 @@ const App = () => {
       <Routes>
         <Route path="/*" element={<HomePage />} />
         <Route path="/streamer" element={<Streamer />} />
-        <Route path="/viewer" element={<Viewer />} />
-        <Route path="/viewer/:channel_name" element={<Viewer />} />
         <Route path="/about" element={<InfoPage />} />
       </Routes>
       <Analytics />
