a93e30321f3c4e1ee98cca7cd47f6e12b9cf85c7
Ollie Boyne <48369287+OllieBoyne@users.noreply.github.com>
2023-09-12 16:46:14 +0100

    fullscreen management

--- src/components/videoJS/videojs.jsx
+++ src/components/videoJS/videojs.jsx
@@ -4,7 +4,9 @@ import { registerIVSTech } from 'amazon-ivs-player';
 import 'video.js/dist/video-js.css';
 import { getStreamLinkFromName } from '../Helpers/APIUtils.jsx';
 import './videojs.css';
+import {FullscreenManager} from './fullscreenManager.jsx';
 
+const fullScreenManager = new FullscreenManager();
 
 const VideoJSPlayer = ({ channel_name, onFullscreenToggle}) => {
   const videoRef = React.useRef(null);
@@ -36,40 +38,8 @@ const VideoJSPlayer = ({ channel_name, onFullscreenToggle}) => {
     const fullscreenBtn = document.querySelector('.vjs-fullscreen-control');
     const videoContainer = document.querySelector('.video-player-container');
 
-    const goFullScreen = () => {
-      const elem = document.querySelector(".video-player-container");
-
-          if (elem.requestFullscreen) {
-            elem.requestFullscreen();
-          } else if (elem.mozRequestFullScreen) { // Firefox
-            elem.mozRequestFullScreen();
-          } else if (elem.webkitRequestFullscreen) { // Chrome, Safari and Opera
-            elem.webkitRequestFullscreen();
-          } else if (elem.msRequestFullscreen) { // IE/Edge
-            elem.msRequestFullscreen();
-          }
-      }
-
-    const endFullscreen = () => {
-      const elem = document.querySelector(".video-player-container");
-
-      if (document.exitFullscreen) {
-        document.exitFullscreen();
-      } else if (document.mozCancelFullScreen) { // Firefox
-        document.mozCancelFullScreen();
-      } else if (document.webkitExitFullscreen) { // Chrome, Safari and Opera
-        document.webkitExitFullscreen();
-      } else if (document.msExitFullscreen) { // IE/Edge
-        document.msExitFullscreen();
-      }
-    }
-
-    const checkIfFullscreen = () => {
-      return document.fullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement || document.msFullscreenElement;
-    }
-
     // set fullscreen status on reload
-    onFullscreenToggle(videoContainer.classList.contains('fullscreen'));
+    onFullscreenToggle(fullScreenManager.status);
 
     if (fullscreenBtn) {
       
@@ -78,14 +48,8 @@ const VideoJSPlayer = ({ channel_name, onFullscreenToggle}) => {
       fullscreenBtn.parentNode.replaceChild(clonedBtn, fullscreenBtn);
   
       clonedBtn.addEventListener('click', function() {
-        const isFullscreen = checkIfFullscreen();
-        if (!isFullscreen) {
-          goFullScreen();
-        } else {
-          endFullscreen();
-        }
-
-        onFullscreenToggle(!isFullscreen);
+        fullScreenManager.toggleFullscreen(videoContainer);
+        onFullscreenToggle(!fullScreenManager.status);
       });
     }
     
@@ -95,7 +59,7 @@ const VideoJSPlayer = ({ channel_name, onFullscreenToggle}) => {
 
   return (
     <video 
-      playsinline
+      playsInline
       ref={videoRef}
       className="video-js vjs-default-skin"
       controls
